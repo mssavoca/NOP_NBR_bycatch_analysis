998ff0b3c9cbc0d8b8abac8ea5e097950c33072e
stephbrodie1 <stephbrodie1@gmail.com>
2019-11-21 17:18:52 -0800

    revised analysis

--- Bycatch_AnalysisCode.R
+++ Bycatch_AnalysisCode.R
@@ -10,6 +10,7 @@ install.packages("mapproj")
 install.packages("ggjoy")
 install.packages("ggpubr")
 install.packages("png")
+install.packages("SDMTools")
 
 library(lattice)
 library(tidyverse)
@@ -19,25 +20,20 @@ library(fiftystater)
 library(ggjoy)
 library(ggpubr)
 library(png)
-
+library("SDMTools")
 
 #------LOAD DATA-------
 #Data can be found on github, contains bycatch summary data for all fisheries and years
-data <- read.csv('SummaryData_August2019_AllFisheryYears.csv', header=T) 
+data <- read.csv('SummaryData_Nov2019_AllFisheryYears.csv', header=T) 
 
-
-
-
-#New column titles:
-colnames(data) <- c("Fishery_ShortName","Fishery","Region","GearType_specific","GearType_general","Year","Target","Target_HMS","Overfishing_Fm","Overfishing_Fm_numeric",
-                    "Overfishing_Bt", "Overfishing_Bt_numeric","TotalBycatch_lbs", "TotalBycatch_inds","Bycatch_ratio",
-                    "ESA_num","ESA_lbs","ESA_birdturt","IUCN_num","IUCN_lbs","IUCN_birdturt",
-                    "MMPA","VessPers","FisheryYears")
+#Invert the Tier scores
+data$Fishery_Tier <- ifelse(data$Fishery_Tier==4,1,
+                            ifelse(data$Fishery_Tier==3,2,
+                                   ifelse(data$Fishery_Tier==2,3,
+                                          ifelse(data$Fishery_Tier==1,4,NA))))
 
 #Normalise data betwen 0 and 1 
 range01 <- function(x, ...){(x - min(x, ...)) / (max(x, ...) - min(x, ...))}
-data$NORM_Overfishing_Fm <- range01(data$Overfishing_Fm_numeric,na.rm=T)
-data$NORM_Overfishing_Bt <- range01(data$Overfishing_Bt_numeric,na.rm=T)
 data$NORM_TotalBycatch_lbs <- range01(data$TotalBycatch_lbs,na.rm=T)
 data$NORM_TotalBycatch_inds <- range01(data$TotalBycatch_inds,na.rm=T)
 data$NORM_Bycatch_ratio <- range01(data$Bycatch_ratio,na.rm=T)
@@ -48,29 +44,27 @@ data$NORM_IUCN_num <- range01(data$IUCN_num,na.rm=T)
 data$NORM_IUCN_lbs <- range01(data$IUCN_lbs,na.rm=T)
 data$NORM_IUCN_birdturt <- range01(data$IUCN_birdturt,na.rm=T)
 data$NORM_MMPA <- range01(data$MMPA,na.rm=T)
-data$NORM_MMPA_2 <- data$NORM_MMPA
-colnames(data)[25:36] <- c("NORM_Overfishing_Fm",
-                           "NORM_Overfishing_Bt", "NORM_TotalBycatch_lbs", "NORM_TotalBycatch_inds","NORM_Bycatch_ratio",
-                           "NORM_ESA_num","NORM_ESA_lbs","NORM_ESA_birdturt","NORM_IUCN_num","NORM_IUCN_lbs","NORM_IUCN_birdturt","NORM_MMPA")
+data$NORM_Tier <- range01(data$Fishery_Tier,na.rm=T)
+data$NORM_CV <- range01(data$CV_mean,na.rm=T)
 
 #-----RELATIVE BYCATCH INDEX------
 #Calculate RBI for each fishery in each year
 #Weighted mean, with MMPA category receiving double weighting
-data$mean_criteria <- apply(data[,25:36],1,function(x) weighted.mean(x,w=c(rep(1,11),2),na.rm=T))
+data$mean_criteria <- apply(data[,28:39],1,function(x) wt.mean(x,wt=c(rep(1,9),2,1,1)))
+data$criteria_sd <- apply(data[,28:39],1,function(x) wt.sd(x,wt=c(rep(1,9),2,1,1)))
+data$ncrit <- as.numeric(apply(data[,28:39],1,function(x) summary(is.na(x))[2]))
+data$criteria_se <- data$criteria_sd / sqrt(data$ncrit)
 
 #List the fisheries with scores in the top 10% 
 unique(data$Fishery_ShortName[data$mean_criteria>quantile(data$mean_criteria, probs = c(0.90), na.rm=T)[1]]) 
 
-
-# Figure 1----
-
-bycatch=read.csv('SummaryData_August2019_AllFisheryYears.csv') %>% 
+#---Figure 1----
+bycatch=read.csv('SummaryData_Nov2019_AllFisheryYears.csv') %>% 
   select(c(GearType_general,Region)) %>% group_by(GearType_general,Region) %>% 
   summarise(count=n()) %>% 
   spread(GearType_general,count) %>% 
   mutate(x=c(-117.5,-68,-105,-74,-130),y=c(19,36,19,23,45)) 
 bycatch[is.na(bycatch)]<-0
-
 
 crimes <- data.frame(state = tolower(rownames(USArrests)), USArrests)
 NE=c("maine","new hampshire","maryland","massachusetts","connecticut", "rhode island","new jersey", "new york", "west virginia","delaware","virginia")
@@ -121,7 +115,7 @@ dev.off()
 
 
 
-# Figure 2 ----
+#---Figure 2----
 # get silhouette images for figures
 imgshark <- png::readPNG("./shark.png")
 rastshark <- grid::rasterGrob(imgshark, interpolate = T)
@@ -234,11 +228,10 @@ dev.copy2pdf(file="Figure_2.pdf", width=14, height=8)
 
 
 
-# Figure 3 ----
+#---Figure 3----
 
 # Get 50 and 75% quantile breaks for all fishery-years scores
 quantile(d1$mean_criteria, probs = c(0.5, 0.75), na.rm = TRUE)
-
 
 # overall histogram of mean criteria score (A)
 mean_score_hist <- ggplot(d1, aes(mean_criteria)) +
@@ -315,30 +308,38 @@ dev.copy2pdf(file="Figure_3.pdf", width=11, height=12)
 
 
 
-# Figure 4----
+#---Figure 4----
 #First select fisheries with 6 years of data
-short <- as.data.frame(table(data$Fishery))
-colnames(short) <- c("Fishery","freq")
-d2 <- left_join(data[,c(1,2,6,38)],short,by="Fishery")
+short <- as.data.frame(table(data$Fishery_ShortName))
+colnames(short) <- c("Fishery_ShortName","freq")
+d2 <- left_join(data[,c(1,2,6,40,43)],short,by="Fishery_ShortName")
+col.pal <- colorRampPalette(c("#053061" ,"#2166AC", "#4393C3",  "#D1E5F0" , "#FDDBC7", "#F4A582" ,"#D6604D" ,"#B2182B","#B2182B","#67001F"))
 tiff('Figure4.tiff',res=300,units="in",width=11,height=14,bg="transparent")
 levelplot(d2$mean_criteria[d2$freq==6]~d2$Year[d2$freq==6]*d2$Fishery_ShortName[d2$freq==6],ylab="",xlab="",main="",scales=list(cex=1, tck=c(1,0)), col.regions=col.pal,panel.abline(v=5))
 dev.off()
 
+#RBI standard error
+tiff('Figure_short_SE.tiff',res=300,units="in",width=11,height=14,bg="transparent")
+col.pal.se <- colorRampPalette(c( "#440154FF", "#31688EFF" ,"#35B779FF", "#FDE725FF" ))
+levelplot(d2$criteria_se[d2$freq==6]~d2$Year[d2$freq==6]*d2$Fishery_ShortName[d2$freq==6],ylab="",xlab="",main="",scales=list(cex=1, tck=c(1,0)), col.regions=col.pal.se,panel.abline(v=5))
+dev.off()
 
-
-#Figure S1 ----
-quartz()
+#---Figure S1 ----
+#RBI
 col.pal <- colorRampPalette(c("#053061" ,"#2166AC", "#4393C3",  "#D1E5F0" , "#FDDBC7", "#F4A582" ,"#D6604D" ,"#B2182B","#B2182B","#67001F"))
 tiff('FigureS1.tiff',res=400,units="in",width=9,height=16,bg="transparent")
 levelplot(data$mean_criteria~data$Year*data$Fishery_ShortName, ylab="",xlab="",main="",scales=list(cex=1, tck=c(1,0)), col.regions=col.pal)
 dev.off()
 
+#RBI standard error
+tiff('Figure_long_SE',res=400,units="in",width=9,height=16,bg="transparent")
+col.pal.se <- colorRampPalette(c( "#440154FF", "#31688EFF" ,"#35B779FF", "#FDE725FF" ))
+levelplot(data$criteria_se~data$Year*data$Fishery_ShortName, ylab="",xlab="",main="",scales=list(cex=1, tck=c(1,0)), col.regions=col.pal.se)
+dev.off()
 
-
-# Figure S2 ----
+#---Figure S2 ----
+tiff('~/PROJECTS/Savoca DOM/Manuscript/Figure_Supp_Criteria_Histograms_R1.tiff',res=300,units="in",width=10,height=7)
 par(mfrow=c(3,4),mar=c(4,2,2,2))
-hist(data$Overfishing_Fm_numeric,main="",ylab="Frequency",xlab="Species subject to overfishing",col="grey")
-hist(data$Overfishing_Bt_numeric,main="",xlab="Species overfished",col="grey")
 hist(data$TotalBycatch_inds,main="",xlab="Seabirds and sea turtles bycaught",col="grey")
 hist(data$TotalBycatch_lbs,main="",xlab="Fish and invertebrates bycaught (lbs)",col="grey")
 hist(data$Bycatch_ratio,main="",ylab="Frequency",xlab="Bycatch Ratio",col="grey")
@@ -349,42 +350,40 @@ hist(data$IUCN_num,main="",xlab="IUCN listed species bycaught",col="grey")
 hist(data$IUCN_lbs,main="",ylab="Frequency",xlab="IUCN listed fish and invertebrates bycaught (lbs)",col="grey")
 hist(data$IUCN_birdturt,main="",xlab="IUCN listed seabirds and sea turtles bycaught",col="grey")
 hist(data$MMPA,main="",xlab="MMPA Category Ranking (inverted)",col="grey")
+hist(data$Fishery_Tier,main="",xlab="Tier Classification System",col="grey")
+hist(data$CV_mean,main="",xlab="Mean Coefficient of Variation",col="grey")
+dev.off()
 
-
-
-
-# Sensitivity analyses, and Figure S3----
+#---Sensitivity analyses, and Figure S3----
 #Sensitivity analysis: find out the relative impact of each criteria on the RBI 
 #This works by randomly changing the criteria value to be itself, or +/- 10%
 #1000 iterations
-sens_anal <- as.data.frame(matrix(NA,nrow=466000,ncol=13)) 
+sens_anal <- as.data.frame(matrix(NA,nrow=462000,ncol=13)) 
 counter=1
 for (i in 1:1000){
-  c1 <- data[,25] + sample(c(1,-0.1,0.1),size=1,replace=TRUE)*data[,25]
-  c2 <- data[,26] + sample(c(1,-0.1,0.1),size=1,replace=TRUE)*data[,26]
-  c3 <- data[,27] + sample(c(1,-0.1,0.1),size=1,replace=TRUE)*data[,27]
-  c4 <- data[,28] + sample(c(1,-0.1,0.1),size=1,replace=TRUE)*data[,28]
-  c5 <- data[,29] + sample(c(1,-0.1,0.1),size=1,replace=TRUE)*data[,29]
-  c6 <- data[,30] + sample(c(1,-0.1,0.1),size=1,replace=TRUE)*data[,30]
-  c7 <- data[,31] + sample(c(1,-0.1,0.1),size=1,replace=TRUE)*data[,31]
-  c8 <- data[,32] + sample(c(1,-0.1,0.1),size=1,replace=TRUE)*data[,32]
-  c9 <- data[,33] + sample(c(1,-0.1,0.1),size=1,replace=TRUE)*data[,33]
-  c10 <- data[,34] + sample(c(1,-0.1,0.1),size=1,replace=TRUE)*data[,34]
-  c11 <- data[,35] + sample(c(1,-0.1,0.1),size=1,replace=TRUE)*data[,35]
-  c12 <- data[,36] + sample(c(1,-0.1,0.1),size=1,replace=TRUE)*data[,36]
+  c1 <- data[,28] + sample(c(1,-0.1,0.1),size=1,replace=TRUE)*data[,28]
+  c2 <- data[,29] + sample(c(1,-0.1,0.1),size=1,replace=TRUE)*data[,29]
+  c3 <- data[,30] + sample(c(1,-0.1,0.1),size=1,replace=TRUE)*data[,30]
+  c4 <- data[,31] + sample(c(1,-0.1,0.1),size=1,replace=TRUE)*data[,31]
+  c5 <- data[,32] + sample(c(1,-0.1,0.1),size=1,replace=TRUE)*data[,32]
+  c6 <- data[,33] + sample(c(1,-0.1,0.1),size=1,replace=TRUE)*data[,33]
+  c7 <- data[,34] + sample(c(1,-0.1,0.1),size=1,replace=TRUE)*data[,34]
+  c8 <- data[,35] + sample(c(1,-0.1,0.1),size=1,replace=TRUE)*data[,35]
+  c9 <- data[,36] + sample(c(1,-0.1,0.1),size=1,replace=TRUE)*data[,36]
+  c10 <- data[,37] + sample(c(1,-0.1,0.1),size=1,replace=TRUE)*data[,37]
+  c11 <- data[,38] + sample(c(1,-0.1,0.1),size=1,replace=TRUE)*data[,38]
+  c12 <- data[,39] + sample(c(1,-0.1,0.1),size=1,replace=TRUE)*data[,39]
   score <- rowMeans(cbind(c1,c2,c3,c4,c5,c6,c7,c8,c9,c10,c11,c12),dims=1)
   
-  c2 = counter+465
-  sens_anal[counter:c2,1:12] <- cbind(c1,c2,c3,c4,c5,c6,c7,c8,c9,c10,c11,c12)
-  sens_anal[counter:c2,13] <- score
-  counter= counter + 466
+  count2 = counter+461
+  sens_anal[counter:count2,1:12] <- cbind(c1,c2,c3,c4,c5,c6,c7,c8,c9,c10,c11,c12)
+  sens_anal[counter:count2,13] <- score
+  counter= counter + 462
 }
-colnames(sens_anal) <- c("Overfishing_Fm",
-                         "Overfishing_Bt", "TotalBycatch_lbs", "TotalBycatch_inds","Bycatch_ratio",
-                         "ESA_num","ESA_lbs","ESA_birdturt","IUCN_num","IUCN_lbs","IUCN_birdturt","MMPA",'Mean_Score')
+colnames(sens_anal) <- c("TotalBycatch_lbs", "TotalBycatch_inds","Bycatch_ratio",
+                         "ESA_num","ESA_lbs","ESA_birdturt","IUCN_num","IUCN_lbs",
+                         "IUCN_birdturt","MMPA","Tier","CV",'Mean_Score')
 #Standardise output
-sens_anal$Overfishing <- (sens_anal$Overfishing_Fm - ((max(sens_anal$Overfishing_Fm,na.rm=T) + min(sens_anal$Overfishing_Fm,na.rm=T)) / 2)) / ((max(sens_anal$Overfishing_Fm,na.rm=T) - min(sens_anal$Overfishing_Fm,na.rm=T)) / 2)
-sens_anal$Overfished <- (sens_anal$Overfishing_Bt - ((max(sens_anal$Overfishing_Bt,na.rm=T) + min(sens_anal$Overfishing_Bt,na.rm=T)) / 2)) / ((max(sens_anal$Overfishing_Bt,na.rm=T) - min(sens_anal$Overfishing_Bt,na.rm=T)) / 2)
 sens_anal$TotalBycatch_lbs <- (sens_anal$TotalBycatch_lbs - ((max(sens_anal$TotalBycatch_lbs,na.rm=T) + min(sens_anal$TotalBycatch_lbs,na.rm=T)) / 2)) / ((max(sens_anal$TotalBycatch_lbs,na.rm=T) - min(sens_anal$TotalBycatch_lbs,na.rm=T)) / 2)
 sens_anal$TotalBycatch_inds <- (sens_anal$TotalBycatch_inds - ((max(sens_anal$TotalBycatch_inds,na.rm=T) + min(sens_anal$TotalBycatch_inds,na.rm=T)) / 2)) / ((max(sens_anal$TotalBycatch_inds,na.rm=T) - min(sens_anal$TotalBycatch_inds,na.rm=T)) / 2)
 sens_anal$Bycatch_ratio <- (sens_anal$Bycatch_ratio - ((max(sens_anal$Bycatch_ratio,na.rm=T) + min(sens_anal$Bycatch_ratio,na.rm=T)) / 2)) / ((max(sens_anal$Bycatch_ratio,na.rm=T) - min(sens_anal$Bycatch_ratio,na.rm=T)) / 2)
@@ -395,14 +394,19 @@ sens_anal$IUCN_species <- (sens_anal$IUCN_num - ((max(sens_anal$IUCN_num,na.rm=T
 sens_anal$IUCN_fish_lbs <- (sens_anal$IUCN_lbs - ((max(sens_anal$IUCN_lbs,na.rm=T) + min(sens_anal$IUCN_lbs,na.rm=T)) / 2)) / ((max(sens_anal$IUCN_lbs,na.rm=T) - min(sens_anal$IUCN_lbs,na.rm=T)) / 2)
 sens_anal$IUCN_birdturtle <- (sens_anal$IUCN_birdturt - ((max(sens_anal$IUCN_birdturt,na.rm=T) + min(sens_anal$IUCN_birdturt,na.rm=T)) / 2)) / ((max(sens_anal$IUCN_birdturt,na.rm=T) - min(sens_anal$IUCN_birdturt,na.rm=T)) / 2)
 sens_anal$MMPA <- (sens_anal$MMPA - ((max(sens_anal$MMPA,na.rm=T) + min(sens_anal$MMPA,na.rm=T)) / 2)) / ((max(sens_anal$MMPA,na.rm=T) - min(sens_anal$MMPA,na.rm=T)) / 2)
+sens_anal$Tier <- (sens_anal$Tier - ((max(sens_anal$Tier,na.rm=T) + min(sens_anal$Tier,na.rm=T)) / 2)) / ((max(sens_anal$Tier,na.rm=T) - min(sens_anal$Tier,na.rm=T)) / 2)
+sens_anal$CV <- (sens_anal$CV - ((max(sens_anal$CV,na.rm=T) + min(sens_anal$CV,na.rm=T)) / 2)) / ((max(sens_anal$CV,na.rm=T) - min(sens_anal$CV,na.rm=T)) / 2)
 
 #Build linear model with output
-sens_m1 <- lm(log(Mean_Score) ~ Overfishing + Overfished + TotalBycatch_lbs + TotalBycatch_inds 
-              +Bycatch_ratio + ESA_species + ESA_fish_lbs + ESA_birdturtle + IUCN_species + IUCN_fish_lbs + IUCN_birdturtle + MMPA,
+sens_m1 <- lm(log(Mean_Score) ~ TotalBycatch_lbs + TotalBycatch_inds +Bycatch_ratio + 
+                ESA_species + ESA_fish_lbs + ESA_birdturtle + IUCN_species + IUCN_fish_lbs
+              + IUCN_birdturtle + MMPA + Tier + CV,
               data = sens_anal)
 #Plot Figure S3: note that random permutations above may change results slightly
 par(mar=c(8,4,2,2))
 barplot(sqrt((sens_m1$coefficients[2:13])^2),las=2,ylab="Relative Impact",cex.axis=0.5)
+
+
 
 
 
